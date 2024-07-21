23c3604c25faaa022992f521853ae00709cbceb6
WINDOWS10 <WINDOWS10@DESKTOP-QIE1G44>
2024-07-21 05:12:35 -0700

    UserManagement

--- AK.Models/ApplicationUser.cs
+++ AK.Models/ApplicationUser.cs
@@ -25,7 +25,9 @@ namespace AK.Models
         public int? CompanyId {  get; set; }
         [ForeignKey("CompanyId")]
         [ValidateNever]
-        public Company Company { get; set; }
+        public Company? Company { get; set; }
+        [NotMapped]
+        public string Role { get; set; }
 
     }
 }
---
+++ AKEcom/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,56 @@
+﻿using AK.DataAccess.Data;
+using AK.DataAccess.Repository.IRepository;
+using AK.Models;
+using AK.Models.ViewModels;
+using AK.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace AKEcom.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles =SD.Role_Admin)]
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UserController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {           
+            return View();
+        }
+
+
+
+       
+        #region APICALL
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<ApplicationUser> objUserList = _db.ApplicationUsers.Include(u => u.Company).ToList();
+            var userroles = _db.UserRoles.ToList();
+            var roles = _db.Roles.ToList();
+            foreach (var user in objUserList)
+            {
+                var roleId = userroles.FirstOrDefault(u => u.UserId == user.Id).RoleId;
+                user.Role = roles.FirstOrDefault(u => u.Id == roleId).Name;
+                if(user.Company == null)
+                {
+                    user.Company = new() { Name = "" };
+                }
+            }
+            return Json(new { data = objUserList });
+        }
+
+        #endregion
+
+
+
+    }
+}
